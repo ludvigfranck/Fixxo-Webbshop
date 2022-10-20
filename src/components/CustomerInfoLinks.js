24dddf420af94feb750899a3ca81556612f2cb64
Ludvig Franck <ludvig.franck@gmail.com>
2022-10-20 11:40:51 +0200

    Fixed CustomerSupportIcons

--- src/components/CustomerInfoLinks.js
+++ src/components/CustomerInfoLinks.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-function CustomerInfoLinks({ icon, title }) {
+function CustomerInfoLinks({ link, icon, title }) {
   return (
     <div className="__customer-info-box">
-        <div className="__customer-info-icon">
+        <NavLink className="__customer-info-icon" to={link}>
             <i className={icon}></i>
-        </div>
+        </NavLink>
         <h1 className="__customer-info-title">{title}</h1>
         <p className="__customer-info-text">Village did removed enjoyed <br /> explain talking.</p>
     </div>
--- src/sections/CustomerInfo.js
+++ src/sections/CustomerInfo.js
@@ -6,10 +6,10 @@ function CustomerInfo() {
     <section className="__customer-info">
         <div className="__customer-info-border"></div>
         <div className="container">
-            <CustomerInfoLinks icon="fa-regular fa-headset" title="Customer Support"></CustomerInfoLinks>
-            <CustomerInfoLinks icon="fa-regular fa-credit-card" title="Secured Payment"></CustomerInfoLinks>
-            <CustomerInfoLinks icon="fa-regular fa-truck" title="Free Home Delivery"></CustomerInfoLinks>
-            <CustomerInfoLinks icon="fa-regular fa-truck" title="30 Day Reuters"></CustomerInfoLinks>
+            <CustomerInfoLinks link="/support" icon="fa-regular fa-headset" title="Customer Support"></CustomerInfoLinks>
+            <CustomerInfoLinks link="/payment" icon="fa-regular fa-credit-card" title="Secured Payment"></CustomerInfoLinks>
+            <CustomerInfoLinks link="/delivery" icon="fa-regular fa-truck" title="Free Home Delivery"></CustomerInfoLinks>
+            <CustomerInfoLinks link="/returns" icon="fa-regular fa-truck" title="30 Day Reuters"></CustomerInfoLinks>
         </div>
     </section>
   )
