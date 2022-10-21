f1786f424afbcd32ce44351da5e983cf9dbe51ab
Ludvig Franck <ludvig.franck@gmail.com>
2022-10-21 10:52:01 +0200

    Added dropdown to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import NavbarLinks from './NavbarLinks'
 
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
   return (
     <nav className="__navbar container">
         <NavLink className="__logo" to="/" end>Fixxo.</NavLink>
-        <div className="__nav-links">
+        <div className={`__nav-links ${ showMenu ? "d-grid" : "" }`}>
             <NavLink className="__nav-link" to="/" end>Home</NavLink>
             <NavLink className="__nav-link" to="/categories" end>Category</NavLink>
             <NavLink className="__nav-link" to="/products" end>Products</NavLink>
@@ -15,9 +21,10 @@ const Navbar = () => {
         </div>
         <div className="__nav-icon-links">
             <NavbarLinks link="/search" icon="fa-regular fa-magnifying-glass" badge="" />
-            <NavbarLinks link="/compare" icon="fa-regular fa-arrows-repeat" badge="" />
-            <NavbarLinks link="/wishlist" icon="fa-regular fa-heart" badge="2" />
+            <NavbarLinks hideOnMobile={true} link="/compare" icon="fa-regular fa-arrows-repeat" badge="" />
+            <NavbarLinks hideOnMobile={true} link="/wishlist" icon="fa-regular fa-heart" badge="2" />
             <NavbarLinks link="/shoppingcart" icon="fa-regular fa-bag-shopping" badge="3" />
+            <button onClick={toggleMenu} className="__nav-icon __btn-nav-icon d-xl-none"><i className="fa-regular fa-bars"></i></button>
         </div>
     </nav>
   )
